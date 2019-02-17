79dfe7e9319d5d5fa89c9938ed612c237371f977
Yana Kryshchuk <kryshchuk@ucu.edu.ua>
2019-02-17 22:47:00 +0200

    created a file

---
+++ read_json.py
@@ -0,0 +1,15 @@
+def read_json(json_, path):
+    """
+    (object, list) -> object
+    Returns a part of json file which is stated in "path".
+    """
+    value = json_[path[0]]
+    if len(path) == 1:
+        return value
+    else:
+        return read_json(value, path[1:len(path)])
+
+# Example of use:
+# api = twitter.Api( consumer_key= "...", consumer_secret= "...", access_token_key= "...", access_token_secret= "..." )
+# json_ = api.GetUser().AsDict()
+# print( read_json( json_, ["status","id"]) )
